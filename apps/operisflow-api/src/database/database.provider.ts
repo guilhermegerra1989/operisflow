300bdc3553291504640772e94ed080e7fa372ea4
Guilherme Geraldo Lemes <seu-email-github>
2026-02-19 16:18:17 -0300

    fix url deploy

--- apps/operisflow-api/src/database/database.provider.ts
+++ apps/operisflow-api/src/database/database.provider.ts
@@ -2,8 +2,8 @@ import { Pool } from 'pg';
 
 export const databaseProvider = {
   provide: 'PG_POOL',
-  useFactory: async () => {
-    console.log('DATABASE_URL EM EXECUÇÃO =>', process.env.DATABASE_URL);
+  useFactory: () => {
+    console.log(">> DATABASE_URL:", process.env.DATABASE_URL);
 
     return new Pool({
       connectionString: process.env.DATABASE_URL,
